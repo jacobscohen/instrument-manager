9e169b379adb1e32592c02a1506aecc4357e0ceb
Jacob Cohen <jacobcohen512@gmail.com>
2024-01-01 12:56:53 -0600

    Initial commit

---
+++ src/test/java/com/example/instrumentmanager/InstrumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instrumentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstrumentManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
