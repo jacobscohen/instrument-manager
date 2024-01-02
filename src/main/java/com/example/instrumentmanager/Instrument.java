b01fc2d46e27870610b13a51150356aab76501d7
Jacob Cohen <jacobcohen512@gmail.com>
2024-01-01 20:44:43 -0600

    Added Instrument fields

--- src/main/java/com/example/instrumentmanager/Instrument.java
+++ src/main/java/com/example/instrumentmanager/Instrument.java
@@ -1,4 +1,33 @@
 package com.example.instrumentmanager;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="instruments")
 public class Instrument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String instrument;
+    private String brandAndModel;
+    private String serialNumber;
+    private String currentOwner;
+    private LocalDate lastRepair;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "`condition`")
+    private Condition condition;
+    private String notes;
+
+    enum Condition {
+        AWFUL, POOR, FAIR, GOOD, EXCELLENT
+    }
 }
